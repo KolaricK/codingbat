0353beaadee2956c502c9bbfab3c8439b5358908
Kristian Kolarić <kkolaric13@gmail.com>
2019-03-28 20:24:56 +0100

    Create sumLimit.java

---
+++ java/logic-1/sumLimit.java
@@ -0,0 +1,13 @@
+/* Given 2 non-negative ints, a and b, return their sum, so long as the sum 
+ * has the same number of digits as a. If the sum has more digits than a, 
+ * just return a without b.
+ */
+ 
+ public int sumLimit(int a, int b) {
+  int sum = a + b;
+        
+    if(String.valueOf(a).length() == String.valueOf(sum).length())
+        return sum;
+                  
+  return a;
+}
