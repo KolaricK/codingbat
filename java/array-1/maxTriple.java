9d38f3c75554794a911e45e97fa4b670d47698a0
Kristian Kolarić <kkolaric13@gmail.com>
2019-03-28 20:39:40 +0100

    Create maxTriple.java

---
+++ java/array-1/maxTriple.java
@@ -0,0 +1,10 @@
+/* Given an array of ints of odd length, look at the first, last, and middle 
+ * values in the array and return the largest. The array length will be a 
+ * least 1.
+ */
+
+public int maxTriple(int[] nums) {
+  int max = Math.max(nums[0], nums[nums.length/2]);
+  max = Math.max(max, nums[nums.length-1]);
+  return max;
+}
