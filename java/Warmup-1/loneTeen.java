c8a2005f7c9981b59366651c5689c2c75d10fe84
Kristian Kolarić <kkolaric13@gmail.com>
2019-03-28 18:19:41 +0100

    Create loneTeen.java

---
+++ java/Warmup-1/loneTeen.java
@@ -0,0 +1,10 @@
+/* We'll say that a number is "teen" if it is in the range 13..19 inclusive. 
+ * Given 2 int values, return true if one or the other is teen, but not both.
+ */
+ 
+ public boolean loneTeen(int a, int b) {
+  boolean teenA = (a >= 13 && a <= 19);
+  boolean teenB = (b >= 13 && b <= 19);
+  
+  return (teenA && !teenB) || (!teenA && teenB);
+}
