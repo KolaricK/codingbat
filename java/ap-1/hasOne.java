b035d2cb5f796ab0fbc089aa9778d1273e9cd0e1
Kristian Kolarić <kkolaric13@gmail.com>
2019-03-28 20:02:11 +0100

    Create hasOne.java

---
+++ java/ap-1/hasOne.java
@@ -0,0 +1,9 @@
+// Given a positive int n, return true if it contains a 1 digit.
+
+ public boolean hasOne(int n) {
+  while (n > 0) {
+    if (n % 10 == 1) return true;
+        n = n / 10;
+  }
+  return false;
+}
