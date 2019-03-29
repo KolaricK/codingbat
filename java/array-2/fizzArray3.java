50bfb5272412f2d592b75a699a6eb224586b8831
Kristian Kolarić <kkolaric13@gmail.com>
2019-03-29 10:52:00 +0100

    Create fizzArray3.java

---
+++ java/array-2/fizzArray3.java
@@ -0,0 +1,17 @@
+/* Given start and end numbers, return a new array containing the sequence of 
+ * integers from start up to but not including end, so start=5 and end=10 
+ * yields {5, 6, 7, 8, 9}. The end number will be greater or equal to the 
+ * start number. Note that a length-0 array is valid.
+ */
+ 
+ public int[] fizzArray3(int start, int end) {
+  int len = end - start;
+  int[] arr = new int[len];
+  
+  for(int i = 0; i < len; i++) {
+    arr[i] = start;
+    start++;
+  }
+  
+  return arr;
+}
